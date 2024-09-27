0702f299d41d9f435d9f5b34e4fc6f5d8cc6b38a
Maxime <max62217@hotmail.fr>
2024-09-27 14:37:00 +0200

    police final

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -21,7 +21,7 @@ import media from './style/media'
 
 const GlobalStyle = createGlobalStyle`
     div {
-        font-family: 'Monteserrat';
+        font-family: 'Montserrat';
     }
 `
 const Contenair = styled.div`
